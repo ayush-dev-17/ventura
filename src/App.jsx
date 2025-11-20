4bd4f99265c7bc79f1c1d332f72542c08587be58
Ayush <ayushsc170197@gmail.com>
2025-11-20 22:19:37 +0530

    fix: rename IPOList.jsx and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import IpoDetails from "./Pages/IpoDetails.jsx";
-import IPOList from "./Pages/IPOList.jsx";
+import IPOList from "./Pages/IPOList";
+// import IPOList from "./Pages/IPOList.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<IPOList />} />
+      <Route  path="/" element={<IPOList/>}></Route>
       <Route path="/ipo/:id" element={<IpoDetails />} />
     </Routes>
   );
